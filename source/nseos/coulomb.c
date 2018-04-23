1575f100850f84939dda9e04ac20f1ac5f853043
Thomas Carreau <thomascarreau7@gmail.com>
2018-04-23 17:53:17 +0200

    Muon energy density, chemical potential, and pressure in coulomb module

--- source/nseos/coulomb.c
+++ source/nseos/coulomb.c
@@ -35,43 +35,79 @@ double calc_screening_derivative(struct parameters satdata, double aa_, double i
     return 0.5*ac*pow(aa_,2./3.)*np_/n0_*(pow(rpt,-2./3.) - 1.);
 }
 
-double calc_egas_energy_density(double np_)
+double calc_egas_energy_density(double ne_)
 {
     double t;
     double egas_energy_density;
 
-    t = HBARC*cpow(3.0*PI2*np_,1./3.)/MEL;
+    t = HBARC*cpow(3.0*PI2*ne_,1./3.)/MEL;
     egas_energy_density = cpow(MEL,4.)/8./PI2/cpow(HBARC,3.)*((2.*t*t+1.)*t*cpow(t*t+1.,1./2.) - log(t + cpow(t*t+1.,1./2.)));
 
     return egas_energy_density;
 }
 
-double calc_egas_chemical_potential(double np_)
+double calc_egas_chemical_potential(double ne_)
 {
     double t;
     double egas_chemical_potential;
 
-    t = HBARC*cpow(3.0*PI2*np_,1./3.)/MEL;
-    egas_chemical_potential = cpow(MEL,3.)/8./cpow(3.*np_*PI2,2./3.)/cpow(HBARC,2.)
+    t = HBARC*cpow(3.0*PI2*ne_,1./3.)/MEL;
+    egas_chemical_potential = cpow(MEL,3.)/8./cpow(3.*ne_*PI2,2./3.)/cpow(HBARC,2.)
         *(cpow(t*t+1.,1./2.)*(1.+6.*t*t) + t*t*(2.*t*t+1.)/cpow(t*t+1.,1./2.)
                 - 1./cpow(t*t+1.,1./2.));
 
     return egas_chemical_potential;
 }
 
-double calc_egas_pressure(double np_)
+double calc_egas_pressure(double ne_)
 {
     double egas_energy_density;
     double egas_chemical_potential;
     double egas_pressure;
 
-    egas_energy_density = calc_egas_energy_density(np_);
-    egas_chemical_potential = calc_egas_chemical_potential(np_);
-    egas_pressure = np_*egas_chemical_potential - egas_energy_density;
+    egas_energy_density = calc_egas_energy_density(ne_);
+    egas_chemical_potential = calc_egas_chemical_potential(ne_);
+    egas_pressure = ne_*egas_chemical_potential - egas_energy_density;
 
     return egas_pressure;
 }
 
+double calc_ugas_energy_density(double nu_)
+{
+    double t;
+    double egas_energy_density;
+
+    t = HBARC*cpow(3.0*PI2*nu_,1./3.)/MMU;
+    egas_energy_density = cpow(MMU,4.)/8./PI2/cpow(HBARC,3.)*((2.*t*t+1.)*t*cpow(t*t+1.,1./2.) - log(t + cpow(t*t+1.,1./2.)));
+
+    return egas_energy_density;
+}
+
+double calc_ugas_chemical_potential(double nu_)
+{
+    double t;
+    double egas_chemical_potential;
+
+    t = HBARC*cpow(3.0*PI2*nu_,1./3.)/MMU;
+    egas_chemical_potential = cpow(MMU,3.)/8./cpow(3.*nu_*PI2,2./3.)/cpow(HBARC,2.)
+        *(cpow(t*t+1.,1./2.)*(1.+6.*t*t) + t*t*(2.*t*t+1.)/cpow(t*t+1.,1./2.)
+                - 1./cpow(t*t+1.,1./2.));
+
+    return egas_chemical_potential;
+}
+
+double calc_ugas_pressure(double nu_)
+{
+    double egas_energy_density;
+    double egas_chemical_potential;
+    double egas_pressure;
+
+    egas_energy_density = calc_egas_energy_density(nu_);
+    egas_chemical_potential = calc_egas_chemical_potential(nu_);
+    egas_pressure = nu_*egas_chemical_potential - egas_energy_density;
+
+    return egas_pressure;
+}
 double calc_lattice_pressure(struct parameters satdata, double aa_, double ii_, double n0_, double np_)
 {
     double rsat;
--- source/nseos/phyconst.h
+++ source/nseos/phyconst.h
@@ -11,6 +11,7 @@
 #define RMN (939.5654133) // neutron mass energy equivalent in MeV
 #define RMP (938.2720813) // proton mass energy equivalent in MeV
 #define MEL (0.5109989461) // electron mass energy equivalent in MeV
+#define MMU (105.6583745) // muon mass energy equivalent in MeV
 #define AMU (931.4940954) // atomic mass constant energy equivalent in MeV
 
 #endif // H_PHYCONST
