27f01747b64f002657ca355d33bf27fdb7f9b540
Thomas Carreau <thomascarreau7@gmail.com>
2018-06-05 18:41:43 +0200

    Delete getPosterior.sh

--- source/apps/stats/getPosterior.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python2 randomSets.py > ../../input/rng_sets.data
-make
-./posterior ../../input/jm_sets.data posterior_ld.out
-./posterior ../../input/mw_sets.data posterior_hd.out
-./posterior ../../input/rng_sets.data posterior_rng.out
